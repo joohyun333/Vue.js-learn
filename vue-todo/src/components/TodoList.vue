1621260dad4329b68b7054213edfac1af139056d
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-02-05 07:29:28 +0900

    Update TodoList.vue

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
         {{ todoItem }}
-        <button v-on:click=""></button>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash"></i>
+        </span>
       </li>
     </ul>
-    list  
   </div>
 </template>
 
@@ -16,6 +17,14 @@ export default {
   data: function(){
     return{
       todoItems:[]
+    }
+  },
+  methods:{
+    removeTodo: function(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      // array.splice(배열의 변경을 시작할 인덱스, 배열에서 제거할 요소의 수, 배열에 추가할 요소(없는 경우 요소 제거만))
+      this.todoItems.splice(index, 1); 
     }
   },
 
@@ -32,6 +41,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
