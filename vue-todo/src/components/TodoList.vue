a421d4e3e442bac7f86ea3f4c91db233a20bbb65
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-02-05 05:55:32 +0900

    todo list 구현중

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+        <button v-on:click=""></button>
+      </li>
+    </ul>
+    list  
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: function(){
+    return{
+      todoItems:[]
+    }
+  },
+
+  // 인스턴스가 생성되자마자 호출되는 라이프 사이클 훅
+  created:function(){
+    if(localStorage.length>0){
+      for(var i =0; i<localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
