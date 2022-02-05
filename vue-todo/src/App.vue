4cb4ff28839325ae7d443d352adc6f46cf310374
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-02-06 06:04:57 +0900

    todolist 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,43 +1,74 @@
 <template>
   <div id="app">
     <todo-header></todo-header>
-    <todo-input></todo-input>
-    <todo-list></todo-list>
+    <!-- v-on:하위 컴포넌트 발생 이벤트 이름="현재 컴포넌트 메서드명" -->
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems"></todo-list>
     <todo-footer></todo-footer>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoList.vue'
-import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
 
 // var my_cmp = {
 // }
 
 export default {
-  components:{ 
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
-  }
-}
+  data: function () {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    addOneItem: function (todoItem) {
+      // 체크됐는지 진위값, text값
+      var obj = { completed: false, item: todoItem };
+      // 저장하는 로직(text값, 자스 객체값 string으로 변환)
+      // localStorage.setItem(this.newTodoItem, obj);
+      // 그냥 obj 기입시 [Object object]로 저장됨(값 확인 X)
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+
+      // 배열에 반영
+      this.todoItems.push(obj);
+    },
+  },
+  // 인스턴스가 생성되자마자 호출되는 라이프 사이클 훅
+  created: function () {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i))),
+          );
+        }
+      }
+    }
+  },
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter,
+  },
+};
 </script>
 
 <style>
-body{
+body {
   text-align: center;
   background-color: #f6f6f6;
 }
 
-input{
+input {
   border-style: groove;
   width: 200px;
 }
 
-button{
+button {
   border-style: groove;
 }
 
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li
+        v-for="(todoItem, index) in propsdata"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="fas fa-check checkBtn"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">
+          {{ todoItem.item }}
+        </span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash"></i>
         </span>
@@ -13,42 +24,34 @@
 
 <script>
 export default {
-
-  data: function(){
-    return{
-      todoItems:[]
-    }
-  },
-  methods:{
-    removeTodo: function(todoItem, index){
-      console.log(todoItem, index);
+  props: ['propsdata'],
+  methods: {
+    removeTodo: function (todoItem, index) {
       localStorage.removeItem(todoItem);
       // array.splice(배열의 변경을 시작할 인덱스, 배열에서 제거할 요소의 수, 배열에 추가할 요소(없는 경우 요소 제거만))
-      this.todoItems.splice(index, 1); 
-    }
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function (todoItem, index) {
+      console.log(index);
+      todoItem.completed = !todoItem.completed;
+      //로컬 스토리지의 데이터를 갱신(update문이 localStorage에 없으므로 없앴다가 다시 업로드해야됨.)
+      // completed:true -> completed:false)
+      // 특정 할일 체크, 미체크 html, localStrage에 업로드
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
   },
-
-  // 인스턴스가 생성되자마자 호출되는 라이프 사이클 훅
-  created:function(){
-    if(localStorage.length>0){
-      for(var i =0; i<localStorage.length; i++){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(localStorage.key(i));
-        }
-      }
-    }
-  }
-}
+};
 </script>
 
 <style scoped>
-ul{
+ul {
   list-style-type: none;
   padding-left: 0px;
   margin-top: 0;
   text-align: left;
 }
-li{
+li {
   display: flex;
   min-height: 50px;
   height: 50px;
@@ -58,19 +61,19 @@ li{
   background: white;
   border-radius: 5px;
 }
-.removeBtn{
+.removeBtn {
   margin-left: auto;
   color: #de4343;
 }
-.checkBtn{
+.checkBtn {
   line-height: 45px;
   color: #62acde;
   margin-right: 5px;
 }
-.checkBtnCompleted{
+.checkBtnCompleted {
   color: #b3adad;
 }
-.textCompleted{
+.textCompleted {
   text-decoration: line-through;
   color: #b3adad;
 }
